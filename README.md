d0c7e91aaf002e99fa5da5d277548bbf6853dd1a
fauzanpr <fauzanpr.06@gmail.com>
2022-10-25 14:07:06 +0700

    spavanilla - Client Side Rendering

---
+++ README.md
@@ -0,0 +1,2 @@
+## About This Repo
+a very simple webpage for implementing Single Page Application using Vanilla Javascript
--- script.js
+++ script.js
@@ -2,20 +2,52 @@ const linkHome = document.getElementById('link-home');
 const linkAbout = document.getElementById('link-about');
 const root = document.getElementById('root');
 
+function HomeScreen() {
+    const input = document.createElement('input');
+    input.placeholder = 'Enter your name';
+
+    const textPreview = document.createElement('p');
+
+    input.oninput = function (event) {
+        textPreview.textContent  = event.target.value;
+    }
+
+    const div = document.createElement('div');
+    div.append(input);
+    div.append(textPreview);
+
+    return div;
+}
+
+function AboutScreen() {
+    const text = document.createElement('p');
+    text.textContent = 'Welcome to About';
+
+    return text;
+}
+
 linkHome.onclick = function (event) {
     event.preventDefault();
+    const homeScreen = HomeScreen();
+    root.innerHTML = '';
+    root.append(homeScreen);
     history.pushState(null, '', event.target.href);
-    root.textContent = 'Welcome to Home';
 };
 
 linkAbout.onclick = function (event) {
     event.preventDefault();
     history.pushState(null, '', event.target.href);
-    root.textContent = 'Welcome to About';
+    const aboutScreen = AboutScreen();
+    root.innerHTML = '';
+    root.append(aboutScreen);
 }
 
 if (location.hash === '#about') {
-    root.textContent = 'Welcome to About';
+    const aboutScreen = AboutScreen();
+    root.innerHTML = '';
+    root.append(aboutScreen);
 } else if (location.hash === '#home') {
-    root.textContent = 'Welcome to Home';
+    const homeScreen = HomeScreen();
+    root.innerHTML = '';
+    root.append(homeScreen);
 }
